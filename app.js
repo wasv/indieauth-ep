7ef3f8bfcff979793ada0bf57d0f27de5f6fe669
Billy Stevens <contact@wasv.me>
2020-07-28 15:24:31 -0400

    Adds index route for debugging auth endpoints locally.

--- app.js
+++ app.js
@@ -16,7 +16,23 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 app.use("/static", express.static(path.join(path.resolve(), "public")));
-
 app.use("/auth", authRouter);
+app.get("/", async (req, res) => {
+  if (process.env.NODE_ENV === "production") {
+    res.sendStatus(404);
+    return;
+  }
+
+  // Allows for testing auth endpoint during development.
+  const host = req.protocol + "://" + req.get("host");
+  const auth_url = new URL(path.join(".", req.baseUrl, "auth"), host);
+  res.send(
+    "<html><head>" +
+      "<link rel='authorization_endpoint' href='" +
+      auth_url +
+      "'/>" +
+      "</head></html>"
+  );
+});
 
 export default app;
